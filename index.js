615c74eb5480c087df6dc6f6517a808f9d99f919
Petros G. Sideris <petros@nmr.com>
2017-01-11 23:39:51 +0000

    make it into a cmd l utility

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+#! /usr/bin/env node
 const sys = require('sys')
 const exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+function puts(error, stdout, stderr) {
+    if(error) throw error
+    sys.puts(stdout)
+}
 exec(`docker rmi $(docker images | grep '^<none>' | awk '{print $3}')`, puts);
